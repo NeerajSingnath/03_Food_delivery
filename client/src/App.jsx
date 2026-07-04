cb38fb1145f35be0b39b1636be252f1ac799397c
Neeraj Singnath <Neerajs604086@gmail.com>
2026-07-04 16:16:41 +0530

    signup structure ready

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/signin" element={<SignIn />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
