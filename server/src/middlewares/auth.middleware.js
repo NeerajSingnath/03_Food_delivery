ab80e60bc21aba9c2a76f1ad812a1daceb373c6b
Neeraj Singnath <Neerajs604086@gmail.com>
2026-07-07 16:16:31 +0530

    feat: implement user authentication middleware and fetch current user controller

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+const isAuth = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(400).json({ message: 'user not authorized' });
+    }
+
+    const verifiedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!verifiedToken) {
+      return res.status(400).json({ message: 'user not authorized' });
+    }
+
+    req.userId = verifiedToken.userId;
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: `user not authorized ${error.message}` });
+  }
+};
